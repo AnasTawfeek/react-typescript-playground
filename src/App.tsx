dc4cdbbb06036918fe03bc988b41ed2df808c5f9
Anas Tawfik <anas.tawfik@booking.com>
2020-03-11 17:06:31 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,71 @@
-import React from 'react';
+import React, { FC, useReducer, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface Todo {
+  content: string
+  done: boolean
+}
+
+interface State {
+    todos: Todo[]
+}
+
+type Action = {
+  type: 'ADD',
+  payload: Todo
+} | {
+  type: 'REMOVE',
+  payload: number
+}
+
+const initialValue: State = {
+  todos: []
+}
+
+const todosReducer = (state: State, action: Action ): State => {
+  switch(action.type) {
+    case 'ADD':
+      return { todos: [
+        ...state.todos,
+        action.payload
+      ]}
+    case 'REMOVE':
+      return { todos: [
+        ...state.todos.filter((todo, i) => i !== action.payload)
+      ]}
+    default:
+      throw new Error();
+  }
+}
+
+const App: FC = () => {
+  const [state, dispatch] = useReducer(todosReducer, initialValue)
+  const [todo, setTodo] = useState('')
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {
+            state.todos.map((todo: Todo, i) => {
+              return (
+                <div key={todo.content}>
+                  <strong>{todo.content}</strong>
+                  <button onClick={() => dispatch({
+                    type: 'REMOVE',
+                    payload: i
+                  })}>X</button>
+                </div>
+              )
+            })
+          }
+        </div>
+        <input value={todo} onChange={e => setTodo(e.target.value)} />
+        <button onClick={() => {
+          dispatch({ type: 'ADD', payload: { content: todo, done: false } })
+          setTodo('')
+        }}>Add</button>
       </header>
     </div>
   );
