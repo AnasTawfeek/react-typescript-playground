1b2a94bdbf23aeb931f0945d2f316f30d02a59f1
Anas Tawfik <anas.tawfik@booking.com>
2020-03-11 17:43:39 +0100

    Add Todo component, styles, cache in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
-import React, { FC, useReducer, useState } from 'react';
+import React, { FC, useReducer, useState, ChangeEvent, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-interface Todo {
+interface ITodo {
   content: string
   done: boolean
 }
 
 interface State {
-    todos: Todo[]
+    todos: ITodo[]
 }
 
 type Action = {
+  type: 'SET_ALL',
+  payload: State
+} | {
   type: 'ADD',
-  payload: Todo
+  payload: ITodo
 } | {
   type: 'REMOVE',
   payload: number
+} | {
+  type: 'EDIT',
+  payload: {
+    index: number,
+    content: string
+  }
 }
 
 const initialValue: State = {
@@ -25,6 +34,8 @@ const initialValue: State = {
 
 const todosReducer = (state: State, action: Action ): State => {
   switch(action.type) {
+    case 'SET_ALL': 
+      return action.payload
     case 'ADD':
       return { todos: [
         ...state.todos,
@@ -34,34 +45,148 @@ const todosReducer = (state: State, action: Action ): State => {
       return { todos: [
         ...state.todos.filter((todo, i) => i !== action.payload)
       ]}
+    case 'EDIT':
+      return { todos: [
+        ...state.todos.map((todo, i) => {
+          if(i === action.payload.index){
+            todo.content = action.payload.content
+          }
+          return todo
+        })
+      ]}
     default:
       throw new Error();
   }
 }
 
+type TodoProps = {
+  todo: ITodo
+  onRemove: any // Not figured out yet
+  onChange: any // Not figured out yet
+}
+
+const styles = {
+  todoInput: {
+    background: 'none',
+    border: 'none',
+    margin: '10px 0',
+    color: '#fff',
+    fontWeight: 700,
+    width: 'calc(100% - 60px)',
+  },
+  todoRemove: {
+    width: '30px',
+    height: '30px',
+    marginLeft: '28px',
+  }
+}
+
+const Todo: FC<TodoProps> = ({ todo, onRemove, onChange }) => {
+  return (
+    <div>
+      <input
+        style={styles.todoInput}
+        value={todo.content}
+        onChange={onChange}
+      />
+      <button
+        style={styles.todoRemove}
+        onClick={onRemove}
+      >X</button>
+    </div>
+  )
+}
+
 const App: FC = () => {
   const [state, dispatch] = useReducer(todosReducer, initialValue)
   const [todo, setTodo] = useState('')
+
+  useEffect(() => {
+    const cache = localStorage.getItem('@myapp/todos')
+    if(cache){
+      dispatch({
+        type: 'SET_ALL',
+        payload: JSON.parse(cache)
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('@myapp/todos', JSON.stringify(state))
+  }, [state])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          {
-            state.todos.map((todo: Todo, i) => {
-              return (
-                <div key={todo.content}>
-                  <strong>{todo.content}</strong>
-                  <button onClick={() => dispatch({
-                    type: 'REMOVE',
-                    payload: i
-                  })}>X</button>
+        <div style={{ display: 'flex', margin: 30 }}>
+          <div>
+            <strong>With Todo component</strong>
+            {
+              state.todos.map((todo: ITodo, i) => (
+                <Todo
+                  key={i}
+                  todo={todo}
+                  onRemove={
+                    () => dispatch({
+                      type: 'REMOVE',
+                      payload: i
+                    })
+                  }
+                  onChange={
+                    (e: ChangeEvent<HTMLInputElement>) => {
+                      dispatch({
+                        type: 'EDIT',
+                        payload: {
+                          index: i,
+                          content: e.target.value
+                        }
+                      })
+                    }
+                  }
+                />
+              ))
+            }
+          </div>
+          <div style={{ marginLeft: 15, paddingLeft: 15, borderLeft: '1px solid rgba(255, 255, 255, 0.2)' }}>
+            <strong>Without Todo component</strong>
+            {
+              state.todos.map((todo: ITodo, i) => (
+                <div key={i}>
+                  <input
+                    style={styles.todoInput}
+                    value={todo.content}
+                    onChange={
+                      (e: ChangeEvent<HTMLInputElement>) => {
+                        dispatch({
+                          type: 'EDIT',
+                          payload: {
+                            index: i,
+                            content: e.target.value
+                          }
+                        })
+                      }
+                    }
+                  />
+                  <button
+                    style={styles.todoRemove}
+                    onClick={
+                      () => dispatch({
+                        type: 'REMOVE',
+                        payload: i
+                      })
+                    }
+                  >X</button>
                 </div>
-              )
-            })
-          }
+              ))
+            }
+          </div>
         </div>
-        <input value={todo} onChange={e => setTodo(e.target.value)} />
+        <input
+          value={todo}
+          onChange={e => setTodo(e.target.value)}
+          placeholder="ex. Play invoker 5th"
+        />
         <button onClick={() => {
           dispatch({ type: 'ADD', payload: { content: todo, done: false } })
           setTodo('')
